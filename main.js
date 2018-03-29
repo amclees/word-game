4ef7d52299589e32395851f6d0a3a5d0bb602830
Andrew McLees <andrewmclees5@gmail.com>
2018-03-28 23:35:05 -0700

    Begin refactor to allow more flexible moves

--- main.js
+++ main.js
@@ -44,8 +44,7 @@ let bonusBoard = [
 ];
 
 let letterBoard = emptyBoard(width, height);
-
-let start = [7, 7];
+let playerBoard = emptyBoard(width, height);
 
 function getWords() {
   let url = './eng.min.json';
@@ -57,7 +56,8 @@ function getWords() {
 }
 
 function resetBoard() {
-  letterBoard = emptyBoard(width, height)
+  letterBoard = emptyBoard(width, height);
+  playerBoard = emptyBoard(width, height);
 }
 
 function emptyBoard(width, height) {
@@ -73,13 +73,16 @@ function emptyBoard(width, height) {
 }
 
 let words = JSON.parse(getWords());
-let wordDictionary = {};
-for (let i = 0; i < words.length; i++) {
-  let sorted = alphabetizeLetters(words[i]);
-  if (!wordDictionary[sorted]) {
-    wordDictionary[sorted] = [];
+let wordTree = {};
+for (let word of words) {
+  let frame = wordTree;
+  for (let i = 0; i < word.length; i++) {
+    if (!frame[word[i]]) {
+      frame[word[i]] = {};
+    }
+    frame = frame[word[i]];
   }
-  wordDictionary[sorted].push(words[i]);
+  frame.end = true;
 }
 
 let boardContainer = document.getElementById('board-container');
@@ -119,6 +122,7 @@ function displayBoard() {
         let input = prompt('Enter the letter');
         if (input === '') {
           letterBoard[x][y] = null;
+          playerBoard[x][y] = null;
           displayBoard();
           return;
         }
@@ -130,8 +134,9 @@ function displayBoard() {
           return;
         }
         letterBoard[x][y] = input.toLowerCase();
+        playerBoard[x][y] = 0;
         displayBoard();
-      }
+      };
       row.appendChild(element);
     }
     table.appendChild(row);
@@ -142,7 +147,58 @@ function displayBoard() {
 
 let letterPoolInput = document.getElementById('letter-pool');
 function getLetterPool() {
-  return alphabetizeLetters(letterPoolInput.value);
+  let pool = {};
+  let inputValue = letterPoolInput.value;
+  for (let letter of inputValue) {
+    pool[letter] = pool[letter] ? pool[letter] + 1 : 1;
+  }
+  return pool;
+}
+
+function getAvailableLetters() {
+  let available = {
+    '*': 2,
+    'e': 12,
+    'a': 9,
+    'i': 9,
+    'o': 8,
+    'n': 6,
+    'r': 6,
+    't': 6,
+    'l': 4,
+    's': 4,
+    'u': 4,
+    'd': 4,
+    'g': 3,
+    'b': 2,
+    'c': 2,
+    'm': 2,
+    'p': 2,
+    'f': 2,
+    'h': 2,
+    'v': 2,
+    'w': 2,
+    'y': 2,
+    'k': 1,
+    'j': 1,
+    'x': 1,
+    'q': 1,
+    'z': 1
+  };
+  for (let letter of getLetterPool()) {
+    available[letter]--;
+  }
+  for (let row of letterBoard) {
+    for (let letter of row) {
+      available[letter]--;
+    }
+  }
+  return available;
+}
+
+let playerCountInput = document.getElementById('player-count-input');
+function getPlayerCount() {
+  return Number.parseInt(playerCountInput.innerHTML);
 }
 
 function normalizeLetters(letters) {
@@ -185,6 +241,8 @@ function combinations(array, amountToChoose) {
   });
 }
 
+let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
 let letterValues = {
   'a': 1,
   'b': 3,
@@ -214,254 +272,111 @@ let letterValues = {
   'z': 10
 };
 
-function scorePlay(play) {
-  let endModifier = 1;
+function inBounds(point) {
+  return point[0] >= 0
+      && point[1] >= 0
+      && point[0] < letterBoard.length
+      && point[1] < letterBoard[0].length;
+};
 
-  let changingDimensionStart = play.playSpot.spot[play.playSpot.direction === 1 ? 0 : 1];
-  for (let i = changingDimensionStart + 1; i < changingDimensionStart + play.word.length; i++) {
-    let bonus = null;
-    if (play.playSpot.direction === 0) {
-      bonus = bonusBoard[play.playSpot.spot[0]][i];
-    } else {
-      bonus = bonusBoard[i][play.playSpot.spot[1]];
-    }
-    if (bonus === dw) {
-      endModifier *= 2;
-    } else if (bonus === tw) {
-      endModifier *= 3;
-    }
+let up = p => [p[0] - 1, p[1]];
+let down = p => [p[0] + 1, p[1]];
+let left = p => [p[0], p[1] - 1];
+let right = p => [p[0], p[1] + 1];
+let directions = [down, right];
+
+function getPlayLine(x, y, direction) {
+  let coords = [[x, y]];
+
+  let current = direction([x, y]);
+  while (inBounds(current)) {
+    coords.push(current);
+    current = direction(current);
   }
 
-  return endModifier * play.word.split('').reduce((score, letter, index) => {
-    let letterBonus = null;
-    if (play.playSpot.direction === 0) {
-      letterBonus = bonusBoard[play.playSpot.spot[0]][changingDimensionStart + index];
-    } else {
-      letterBonus = bonusBoard[changingDimensionStart + index][play.playSpot.spot[1]];
-    }
-
-    let letterModifier = 1;
-    if (letterBonus === dl) {
-      letterModifier = 2;
-    } else if (letterBonus === tl) {
-      letterModifier = 3;
-    }
-    if (index === 0) {
-      letterModifier = 1;
-    }
-    return score + (letterValues[letter] * letterModifier);
-  }, 0);
-}
-
-function spotEmpty(x, y, outOfBounds = false) {
-  if (outOfBounds) {
-    return (!(x >= 0
-      && y >= 0
-      && x < letterBoard.length
-      && y < letterBoard[0].length))
-      || letterBoard[x][y] === null;
-  } else {
-    return x >= 0
-      && y >= 0
-      && x < letterBoard.length
-      && y < letterBoard[0].length
-      && letterBoard[x][y] === null;
-  }
-}
-
-function adjacent(x, y) {
-  let adjacents = [];
-  for (let i = -1; i <= 1; i++) {
-    for (let j = -1; j <= 1; j++) {
-      let adjacentX = x + i;
-      let adjacentY = y + j;
-      if (
-        adjacentX >= 0
-        && adjacentX < letterBoard.length
-        && adjacentY >= 0
-        && adjacentY < letterBoard[0].length
-      ) {
-        adjacents.push([adjacentX, adjacentY]);
-      }
-    }
-  }
-  return adjacents;
-}
-
-function spotClear(x, y, whitelisted) {
-  if (!whitelisted) { whitelisted = []; }
-
-  if (
-    !(x >= 0
-    && x < letterBoard.length
-    && y >= 0
-    && y < letterBoard[0].length)
-  ) {
-    return false;
-  }
-
-  return adjacent(x, y).reduce(function(acc, adjacent) {
-    return acc && (spotEmpty(adjacent[0], adjacent[1]) || whitelisted.reduce(function(acc, whitelisted) {
-      return acc || (whitelisted[0] === adjacent[0] && whitelisted[1] == adjacent[1])
-    }, false));
-  }, true);
-}
-
-function getPlaySpots() {
-  let playSpots = [];
-  for (let i = 0; i < letterBoard.length; i++) {
-    for (let j = 0; j < letterBoard[0].length; j++) {
-      Array.prototype.push.apply(playSpots, playSpotsAtLocation(i, j));
-    }
-  }
-  return playSpots;
-}
-
-function playSpotsAtLocation(i, j) {
-  let spot = false,
-      spotSize = 1,
-      playSpots = [];
-
-  if (letterBoard[i][j] !== null) {
-    // Length of spots includes the existing letter
-    if (
-      spotClear(
-        i, j + 1, [[i, j], [i - 1, j], [i + 1, j]]
-      )
-      && spotEmpty(i, j - 1, true)
-    ) {
-      let whitelist = [[i, j], [i - 1, j], [i + 1, j]];
-      for (let k = j + 1; k < letterBoard[0].length; k++) {
-        if (!spotClear(i, k, whitelist)) {
-          break;
-        }
-        spotSize++;
-        whitelist.push([i, k]);
-      }
-
-      playSpots.push({
-        spot: [i, j],
-        letter: letterBoard[i][j],
-        // Directions are 0, 1 for right and down respectively
-        direction: 0,
-        length: spotSize
-      });
-    }
-    spotSize = 1;
-    if (
-      spotClear(
-        i + 1, j, [[i, j], [i, j - 1], [i, j + 1]]
-      )
-      && spotEmpty(i - 1, j, true)
-    ) {
-      let whitelist = [[i, j], [i, j - 1], [i, j + 1]];
-      for (let k = i + 1; k < letterBoard.length; k++) {
-        if (!spotClear(k, j, whitelist)) {
-          break;
-        }
-        spotSize++;
-        whitelist.push([k, j]);
-      }
-
-      playSpots.push({
-        spot: [i, j],
-        letter: letterBoard[i][j],
-        direction: 1,
-        length: spotSize
-      });
-    }
-  }
-
-  return playSpots;
-}
-
-function getPlays(playSpots, letterPool) {
-  let wildcards = letterPool.length - letterPool.join('').replace(/\*/g, '').length;
-
-  let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  for (let w = 0; w < wildcards - 1; w++) {
-    Array.prototype.push.apply(alphabet, alphabet);
-  }
-  let wildcardCombinations = combinations(alphabet, wildcards).map((string) => {
-    return string.split('');
-  });
-
-  letterPool = lettersOnly(letterPool);
-
-  let plays = [];
-  for (let i = 0; i < playSpots.length; i++) {
-    let playSpot = playSpots[i];
-    for (let length = playSpot.length; length >= 2; length--) {
-      for (let c = 0; c < wildcardCombinations.length; c++) {
-        let wordBases = combinations(letterPool.concat(wildcardCombinations[c]), length - 1);
-        let used = {};
-
-        for (let d = 0; d < wordBases.length; d++) {
-          let alphabetized = alphabetizeLetters(playSpot.letter + wordBases[d]);
-          if (used[alphabetized] !== undefined) { continue; }
-          used[alphabetized] = true;
-
-          let words = wordDictionary[alphabetized];
-          if (words === undefined) { continue; }
-          for (let e = 0; e < words.length; e++) {
-            if (words[e].charAt(0) !== playSpot.letter) { continue; }
-            plays.push({
-              'word': words[e],
-              'playSpot': playSpot
-            });
-          }
-        }
-      }
-    }
-  }
-  return plays;
-}
-
-function choosePlay(plays) {
-  let scores = plays.map(scorePlay);
-  let highScore = -1;
-  let toReturn = null;
-  for (let i = 0; i < plays.length; i++) {
-    if (scores[i] > highScore) {
-      toReturn = plays[i];
-      highScore = scores[i];
-    }
-  }
-  return toReturn;
-}
-
-function applyPlay(play) {
-  let changingDimensionStart = play.playSpot.spot[play.playSpot.direction === 1 ? 0 : 1];
-  for (let i = changingDimensionStart; i < changingDimensionStart + play.word.length; i++) {
-    if (play.playSpot.direction === 0) {
-      letterBoard[play.playSpot.spot[0]][i] = play.word.charAt(i - changingDimensionStart);
-    } else {
-      letterBoard[i][play.playSpot.spot[1]] = play.word.charAt(i - changingDimensionStart);
-    }
-  }
-  displayBoard();
-}
-
-function getLineCoords(start, end) {
-  let coords = [];
-
-  for (let x = start[0]; x <= end[0]; x++) {
-    for (let y = start[1]; y <= end[1]; y++) {
-      coords.push([x, y]);
-    }
-  }
+  coords.direction = direction;
 
   return coords;
 }
 
-function validateLine(start, end, validator) {
-  let coords = getLineCoords(start, end);
-
-  let valid = true;
-  for (let i = 0; i < coords.length; i++) {
-    valid = valid && validator(coords[i]);
+// Needs reverse mode for up/left play
+// Needs modifiers
+function maxSpotPlay(playLine, i, score, string, letterPool, branch, newLetter) {
+  if (i === playLine.length && newLetter) {
+    return [score, string];
   }
-  return valid;
+
+  let existingValue = letterBoard[playLine[i][0]][playLine[i][1]];
+  if (existingValue !== null) {
+    if (branch[existingValue]) {
+      return maxSpotPlay(playLine, i + 1, score + letterValues[existingValue], string + existingValue, letterPool, branch[existingValue], newLetter);
+    } else {
+      return [-Infinity, ''];
+    }
+  }
+
+  let bestScore = -Infinity;
+  let bestString = '';
+
+  if (branch.end && newLetter) {
+    bestScore = score;
+    bestString = string;
+  }
+
+  for (let letter in branch) {
+    if (letterPool[letter] && letterPool[letter] > 0) {
+      letterPool[letter]--;
+      let maxWithLetter = maxSpotPlay(playLine, i + 1, score + letterValues[letter], string + letter, letterPool, branch[letter], true);
+      letterPool[letter]++;
+      if (maxWithLetter[0] > bestScore) {
+        bestScore = maxWithLetter[0];
+        bestString = maxWithLetter[1];
+      }
+    }
+  }
+  return [bestScore, bestString];
+}
+
+function tryLine(playLine, letterPool) {
+  let firstLetter = letterBoard[playLine[0][0]][playLine[0][1]]
+  let bestPlay = maxSpotPlay(playLine, 1, 0, firstLetter, letterPool, wordTree[firstLetter], false);
+
+  return bestPlay;
+}
+
+function play() {
+  let letterPool = getLetterPool();
+
+  let playLines = [];
+  for (let i = 0; i < letterBoard.length; i++) {
+    for (let j = 0; j < letterBoard[0].length; j++) {
+      if (letterBoard[i][j] == null) {
+        continue;
+      }
+      for (let direction of directions) {
+        let line = getPlayLine(i, j, direction);
+        if (line.length !== 0) {
+          playLines.push(line);
+        }
+      }
+    }
+  }
+
+  let bestLine = null;
+  let bestString = '';
+  let bestScore = -1;
+  for (let playLine of playLines) {
+    let move = tryLine(playLine, letterPool);
+    if (move[0] > bestScore) {
+      bestLine = playLine;
+      bestScore = move[0];
+      bestString = move[1];
+    }
+  }
+
+  for (let i = 0; i < bestString.length; i++) {
+    letterBoard[bestLine[i][0]][bestLine[i][1]] = bestString[i];
+  }
 }
 
 let resetButton = document.getElementById('reset-button');
@@ -472,14 +387,8 @@ resetButton.onclick = function() {
 
 let placementButton = document.getElementById('placement-button');
 placementButton.onclick = function() {
-  let letterPool = getLetterPool();
-  if (letterPool.length === 0) { return; }
-
-  let playSpots = getPlaySpots();
-  let plays = getPlays(playSpots, letterPool);
-  let play = choosePlay(plays);
-
-  applyPlay(play);
+  play();
+  displayBoard();
 };
 
 displayBoard();
